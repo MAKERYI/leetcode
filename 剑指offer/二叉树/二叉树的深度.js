29e0d7064130fb1e79336163375e6f37c6ecbdf3
MAKERYI <github@makeryi.top>
2021-03-25 22:09:49 +0800

    keep

---
+++ 剑指offer/二叉树/二叉树的深度.js
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+
+// DFS
+function getDepth(root) {
+  if (!root) return 0
+  return Math.max(getDepth(root.left), getDepth(root,right)) + 1
+}
+
+// BFS
+
+function getDepth(root) {
+  if (!root) return 0
+  let queue = [root]
+  let res = 0
+  
+  while (queue.length) {
+    let levelNum = queue.length
+    res++
+    while (levelNum--) {
+      const node = queue.shift()
+      if (node.left) queue.push(node.left)
+      if (node.right) queue.push(node.right)
+    }
+  }
+
+  return res
+}
